366953f6c656add42b3ac7846b8562ca19ec31a4
Zoe Hendershot <njd5rd@virginia.edu.com>
2024-11-24 20:04:40 +0000

    add script 1 to lab folder

---
+++ lab8/parsejsonwjq.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -s "https://aviationweather.gov/api/data/metar?ids=KMCI&format=json&taf=false&hours=12&bbox=40%2C-90%2C45%2C-85" > aviation.json
+
+jq '.[].receiptTime' aviation.json | head -n 6
